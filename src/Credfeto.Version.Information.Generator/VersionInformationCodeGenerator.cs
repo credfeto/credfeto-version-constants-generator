571ce08bb2a5331035991565011734999e3a8119
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-10-17 12:12:45 +0100

    Common code

--- src/Credfeto.Version.Information.Generator/VersionInformationCodeGenerator.cs
+++ src/Credfeto.Version.Information.Generator/VersionInformationCodeGenerator.cs
@@ -40,8 +40,7 @@ public sealed class VersionInformationCodeGenerator : ISourceGenerator
               .AppendBlankLine()
               .AppendLine($"namespace {assemblyNamespace};")
               .AppendBlankLine()
-              .AppendGeneratedCodeAttribute()
-              .AppendLine($"[GeneratedCode(tool: \"{RuntimeVersionInformation.ToolName}\", version: \"{RuntimeVersionInformation.GeneratorVersion}\")]");
+              .AppendGeneratedCodeAttribute();
 
         using (source.StartBlock("internal static class VersionInformation"))
         {
